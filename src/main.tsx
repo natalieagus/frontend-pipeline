570f83c4df3cf7e855b0e3d5950bb3590a458669
Natalie Agus <nn88blue@gmail.com>
2023-01-16 22:54:00 +0800

    fix: router with basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'frontend-pipeline'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
